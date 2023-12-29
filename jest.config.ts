725913785b8beaa8c2149fd48cea9c5288ebc41c
novaotp <rahmansajidur30@gmail.com>
2023-12-29 22:45:54 +0100

    Added path aliases + moved tests to root dir

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,10 @@ const config: JestConfig = {
     testEnvironment: 'node',
     transform: {},
     transformIgnorePatterns: [],
-    moduleDirectories: ['node_modules', 'src']
+    moduleDirectories: ['node_modules', 'src'],
+    moduleNameMapper: {
+        '^@frontend/(.*)$': '<rootDir>/src/frontend/$1',
+    }
 }
   
 export default config;
